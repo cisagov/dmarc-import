d7951631e63cd5d71d370a7ba8d0fc621731032f
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-05-15 17:11:21 -0400

    Adding another logging statement to aid debugging

--- dmarc/s3.py
+++ dmarc/s3.py
@@ -410,6 +410,7 @@ class Parser:
                                 # We can't query the Dmarcian API because we
                                 # don't have a token, so just add an empty
                                 # entry
+                                logging.debug('json is: {}'.format(jsn))
                                 logging.debug('record is: {}'.format(record))
                                 record['row']['source_ip_affiliation'] = None
 
